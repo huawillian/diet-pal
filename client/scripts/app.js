36779f62f99014b9f8676cf794f1d7be419c436c
huawillian <huawillian@gmail.com>
2017-10-24 01:02:44 -0400

    added line graph for weight over time

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,10 +5,25 @@ app.controller('mainController', function($scope, $http, entriesApi) {
     $scope.entryValue = "";
     $scope.data = {};
 
+    let ctx = angular.element(document.getElementById("myChart").getContext('2d'));
+
     $scope.fetchData = () => {
       entriesApi.getEntries()
         .then(function(data) {
-            $scope.data = data;
+            $scope.data = data.sort((a,b) => a.date > b.date);
+
+            console.log($scope.data);
+
+            let myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+              labels: $scope.data.map(o => o.date),
+              datasets: [{
+                label: 'weight (lbs)',
+                data: $scope.data.map(o => o.weight),
+              }]
+              }
+            });
         });
     }
 
